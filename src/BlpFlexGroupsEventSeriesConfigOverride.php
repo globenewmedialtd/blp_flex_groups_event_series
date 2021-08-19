cb480974f035d03620de2c3e0d9ecf9199562276
globenewmedialtd <globenewmedialtd@gmail.com>
2021-08-19 12:21:52 +0200

    Next release

--- src/BlpFlexGroupsEventSeriesConfigOverride.php
+++ src/BlpFlexGroupsEventSeriesConfigOverride.php
@@ -95,11 +95,6 @@ class BlpFlexGroupsEventSeriesConfigOverride implements ConfigFactoryOverrideInt
     foreach ($config_names as $config_name) {
       if (in_array($config_name, $names)) {
         $overrides[$config_name] = [
-          'dependencies' => [
-            'config' => [
-              'blp_event-group_membership' => 'group.content_type.blp_event-group_membership',
-            ],
-          ],
           'display' => [
             'default' => [
               'display_options' => [
@@ -125,12 +120,6 @@ class BlpFlexGroupsEventSeriesConfigOverride implements ConfigFactoryOverrideInt
     foreach ($config_names as $config_name => $content_type) {
       if (in_array($config_name, $names)) {
         $overrides[$config_name] = [
-          'dependencies' => [
-            'config' => [
-              'group-content-type' => 'group.content_type.' . $content_type,
-              'group-type' => 'group.type',
-            ],
-          ],
           'display' => [
             'default' => [
               'display_options' => [
